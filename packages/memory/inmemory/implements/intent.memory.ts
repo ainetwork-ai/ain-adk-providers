abb7d89747d7893962dd0653443b3dfb9fbbb448
Seonghwa Yun <seonghwa.yun@comcom.ai>
2025-07-31 18:44:25 +0900

    fix

---
+++ packages/memory/inmemory/implements/intent.memory.ts
@@ -0,0 +1,34 @@
+import { randomUUID } from "node:crypto";
+import type { Intent } from "@ainetwork/adk/types/memory";
+import { IIntentMemory } from "@ainetwork/adk/modules";
+
+export class InMemoryIntent implements IIntentMemory {
+	public intents: Map<string, Intent> = new Map();
+
+  public async connect(): Promise<void> {}
+  public async disconnect(): Promise<void> {}
+  public isConnected(): boolean {
+    return true;
+  }
+
+  public async getIntent(intentId: string): Promise<Intent | undefined> {
+    return this.intents.get(intentId);
+  };
+
+	public async saveIntent(intent: Intent): Promise<void> {
+    const newId = randomUUID();
+    this.intents.set(newId, intent);
+  };
+
+	public async updateIntent(intentId: string, intent: Intent): Promise<void> {
+    this.intents.set(intentId, intent);
+  };
+
+	public async deleteIntent(intentId: string): Promise<void> {
+    this.intents.delete(intentId);
+  };
+
+	public async listIntents(): Promise<Intent[]> {
+    return Array.from(this.intents.values());
+  };
+}
---
+++ packages/memory/inmemory/implements/session.memory.ts
@@ -0,0 +1,38 @@
+import { randomUUID } from "node:crypto";
+import type { ChatObject, SessionObject } from "@ainetwork/adk/types/memory";
+import { ISessionMemory } from "@ainetwork/adk/modules";
+
+export class InMemorySession implements ISessionMemory {
+	public sessions: Map<string, SessionObject> = new Map();
+
+  public async connect(): Promise<void> {}
+  public async disconnect(): Promise<void> {}
+  public isConnected(): boolean {
+    return true;
+  }
+
+  public async getSession(sessionId: string): Promise<SessionObject | undefined> {
+    return this.sessions.get(sessionId);
+  };
+
+	public async createSession(sessionId: string): Promise<void> {
+    this.sessions.set(sessionId, { chats: {} });
+  };
+
+	public async addChatToSession(sessionId: string, chat: ChatObject): Promise<void> {
+    const newChatId = randomUUID();
+    const session = await this.getSession(sessionId);
+    if (session) {
+      session.chats[newChatId] = chat;
+    }
+  };
+
+	public async deleteSession(sessionId: string): Promise<void> {
+    this.sessions.delete(sessionId);
+  };
+
+	public async listSessions(): Promise<string[]> {
+    const sessionIds = Array.from(this.sessions.keys());
+    return sessionIds;
+  };
+}

822f729e25e8f85c23ec913d58879a2f545d0be7
Seonghwa Yun <seonghwa.yun@comcom.ai>
2026-01-05 15:12:39 +0900

    fix console error log

--- packages/auth/otp/implements/otp.auth.ts
+++ packages/auth/otp/implements/otp.auth.ts
@@ -64,7 +64,7 @@ export class OTPAuth extends BaseAuth {
         return { isAuthenticated: false };
       }
     } catch (err) {
-      console.error("Google auth verification failed:", (err as Error).message);
+      console.error("OTP auth verification failed:", (err as Error).message);
       return { isAuthenticated: false };
     }
   }
